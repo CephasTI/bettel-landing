e235d86b7941ec012230c95158f8d3296caa0509
Pedro Rodrigues <pedrorodfer5@gmail.com>
2026-09-08 18:58:09 -0300

    fix: Home display bugs is fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 import Hero from "./Hero/Hero";
 import About from "./About/About";
 
 function Home(){
     return(
         <div>
+            <Header/>
+
             <Hero/>
             <About/>
+
+            <Footer/>
         </div>
     )
 }
